2f2a3be3840315fa906aa5df2e9f05d76e2e5715
wcc723 <agito723@gmail.com>
2019-10-25 14:39:46 +0800

    add let, const

---
+++ 07. ES6 - Let, Const/04-eslint.js
@@ -0,0 +1,10 @@
+// ESLint 安裝教學：https://www.youtube.com/watch?v=WiommcFaYvY
+
+// 原文：https://github.com/airbnb/javascript
+// 中文：https://github.com/yuche/javascript#variables
+
+var a = 'a';
+
+// 'obj' is never reassigned. Use 'const'
+let obj = {};
+obj.a = 'a';
